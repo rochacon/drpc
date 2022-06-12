c10c2ae1bca8b4237a43cfa0b704e0f4d6baf119
Rodrigo Chacon <rochacon@kumbuca.com>
2022-06-12 15:01:19 -0300

    example/drpc: proper load balancing, stats per worker and debug logs
    Signed-off-by: Rodrigo Chacon <rochacon@kumbuca.com>

--- examples/drpc/client/main.go
+++ examples/drpc/client/main.go
@@ -21,11 +21,12 @@ import (
 
 func main() {
 	var addr string
-	var concurrency, requests int
-	var tlsEnabled, verbose bool
+	var concurrency, requests uint
+	var debug, tlsEnabled, verbose bool
 	flag.StringVar(&addr, "a", "127.0.0.1:8080", "address to dial to")
-	flag.IntVar(&concurrency, "c", 1, "worker concurrency")
-	flag.IntVar(&requests, "n", 1, "number of requests to be made")
+	flag.UintVar(&concurrency, "c", 1, "worker concurrency")
+	flag.BoolVar(&debug, "d", false, "print debug logs")
+	flag.UintVar(&requests, "n", 1, "number of requests to be made")
 	flag.BoolVar(&tlsEnabled, "tls", false, "enable TLS")
 	flag.BoolVar(&verbose, "v", false, "print result for each call")
 	flag.Parse()
@@ -34,27 +35,50 @@ func main() {
 		DisableColors: true,
 		FullTimestamp: true,
 	})
+	if debug {
+		log.SetLevel(log.DebugLevel)
+	}
 
-	latency := make(chan int64, concurrency*requests)
+	// start stats aggregator
+	latency := make(chan Latency, concurrency*requests)
 	stats := Stats{
-		Start: time.Now(),
+		CallsPerWorker:      make([]uint64, concurrency),
+		Start:               time.Now(),
+		LatencySumPerWorker: make([]uint64, concurrency),
 	}
 	go func() {
 		for l := range latency {
 			stats.Calls += 1
-			stats.LatencySum += l
+			stats.CallsPerWorker[l.Worker] += 1
+			stats.LatencySum += uint64(l.Elapsed)
+			stats.LatencySumPerWorker[l.Worker] += uint64(l.Elapsed)
 		}
+		log.Debug("latency channel closed")
 	}()
 
+	// start work queue
+	work := make(chan uint)
+	go func() {
+		for rc := uint(0); rc < requests; rc++ {
+			work <- rc
+		}
+		close(work)
+		log.WithFields(log.Fields{
+			"requests": requests,
+		}).Debug("finished queuing requests")
+	}()
+
+	// start workers
 	log.WithFields(log.Fields{
 		"addr":        addr,
 		"concurrency": concurrency,
 		"requests":    requests,
 	}).Printf("starting workers")
+
 	wg := &sync.WaitGroup{}
-	for id := 0; id < concurrency; id++ {
+	for id := uint(0); id < concurrency; id++ {
 		wg.Add(1)
-		go func(wg *sync.WaitGroup, requests int, id int, latency chan<- int64) {
+		go func(wg *sync.WaitGroup, work <-chan uint, id uint, latency chan<- Latency) {
 			defer wg.Done()
 			var err error
 			var rawconn drpc.Transport
@@ -69,36 +93,46 @@ func main() {
 				os.Exit(1)
 			}
 			defer rawconn.Close()
-			log.WithFields(log.Fields{"worker": id, "elapsed": time.Since(connStart)}).Println("connection started")
+			log.WithFields(log.Fields{
+				"elapsed": time.Since(connStart),
+				"worker":  id,
+			}).Debug("connection started")
 
 			conn := drpcconn.New(rawconn)
 			defer conn.Close()
 
 			client := pb.NewDRPCCookieMonsterClient(conn)
 
-			for rc := 0; rc < requests; rc++ {
+			for range work {
 				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-				defer cancel()
 
 				start := time.Now()
 				crumbs, err := client.EatCookie(ctx, &pb.Cookie{
 					Type: pb.Cookie_Chocolate,
 				})
 				if err != nil {
-					log.WithFields(log.Fields{"worker": id, "error": err}).Println("failed")
+					log.WithFields(log.Fields{
+						"error":  err,
+						"worker": id,
+					}).Println("failed")
+					cancel()
 					continue
 				}
 				elapsed := time.Since(start)
-				latency <- int64(elapsed)
+				latency <- Latency{
+					Elapsed: elapsed,
+					Worker:  id,
+				}
 				if verbose {
 					log.WithFields(log.Fields{
-						"worker":  id,
 						"cookie":  crumbs.Cookie.Type.String(),
 						"latency": elapsed.String(),
+						"worker":  id,
 					}).Println("EatCookie")
 				}
+				cancel()
 			}
-		}(wg, requests, id, latency)
+		}(wg, work, id, latency)
 	}
 	wg.Wait()
 	close(latency)
@@ -107,14 +141,32 @@ func main() {
 		"latency_mean": stats.LatencyMean().String(),
 		"elapsed":      time.Since(stats.Start).String(),
 	}).Println("stats")
+	for i, n := range stats.CallsPerWorker {
+		log.WithFields(log.Fields{
+			"calls":        n,
+			"latency_mean": stats.WorkerLatencyMean(uint(i)).String(),
+			"worker":       i,
+		}).Println("stats per worker")
+	}
+}
+
+type Latency struct {
+	Elapsed time.Duration
+	Worker  uint
 }
 
 type Stats struct {
-	Calls      int64
-	LatencySum int64
-	Start      time.Time
+	Calls               uint64
+	CallsPerWorker      []uint64
+	LatencySum          uint64
+	LatencySumPerWorker []uint64
+	Start               time.Time
 }
 
 func (s Stats) LatencyMean() time.Duration {
 	return time.Duration(s.LatencySum / s.Calls)
+}
+
+func (s Stats) WorkerLatencyMean(id uint) time.Duration {
+	return time.Duration(s.LatencySumPerWorker[id] / s.CallsPerWorker[id])
 }
